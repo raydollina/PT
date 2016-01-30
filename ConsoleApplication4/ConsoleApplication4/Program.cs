5c2607691c6a34c7f0a0f8f6a5dd19d03cdc4cdd
Маржан Райдоллина < >
2016-01-30 21:13:01 +0600

    prime numbers

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static bool IsPrime(string s)
+        {
+            //конвертация от типа строки в тип целое число
+            int x = int.Parse(s);
+            int cnt = 0;
+            //подсчет кол-ва делителей отличных от 1 и самого числа
+            for (int j = 2; j <= Math.Sqrt(x); ++j)
+            {
+                if (x % j == 0)
+                {
+                    cnt++;
+                }
+            }
+
+            return cnt == 0 && x != 1;
+        }
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/prime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] token = Console.ReadLine().Split();   
+            for (int n = 0; n < token.Length; n++)
+            {
+                int qwe = 0; 
+                int b = int.Parse(token[n]); // изменить на цифры
+                for (int i = 1; i <= b; i++) // число от 1 до заданного числа
+                {
+                    if (b % i == 0) // делиться ли число на что либо
+                    {
+                        qwe++; 
+                    }
+                }
+                if (cnt < 3) // если число делителей меньше 3, значит число делителей всего 2, это значит число делиться на себя и на 1
+                {
+                    Console.Write("{0} ", b); // вывести простое число                   
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
